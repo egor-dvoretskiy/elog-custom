47513c141739928aef3ccacdc3ed97969ee347c6
ergo.dvoretskiy <ergo.dvoretskiy@gmail.com>
2022-09-13 16:33:59 +0300

    Add nlog tests.

---
+++ ELogLib/ELogBenchmark/CustomRun/NLogTests.cs
@@ -0,0 +1,82 @@
+﻿using EMonolithLib.Extensions.Timing;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELogBenchmark.CustomRun
+{
+    public class NLogTests
+    {
+        private readonly ILogger _logger2 = LogManager.GetCurrentClassLogger();
+        private readonly List<string> _args = new List<string>()
+        {
+            "Simple line.",
+            "big apdosjdpasdnop3412-490kdasdkaslk_++ASD+_A_SDASD+AD!212ojasdstrigndasasdasdat34t34vt3vt4u686769700-90.786m63b532v41        asDASD!+@$!@+#$!@_%#_$^$&*%&*^*(",
+            "line with signs \nhello=16\n\rsdad= 12\n\n\t\t24 = 24."
+        };
+
+        public NLogTests()
+        {
+        }
+
+        public void Run()
+        {
+            this.PrintHeader(this._args[0]);
+            this.Test(this._args[0]);
+
+            this.PrintHeader(this._args[1]);
+            this.Test(this._args[1]);
+
+            this.PrintHeader(this._args[2]);
+            this.Test(this._args[2]);
+        }
+
+        public void Test(string msg)
+        {
+            List<double> resultTest = new List<double>();
+            int testCount = 130;
+
+            for (int i = 0; i < testCount; i++)
+            {
+                this.EvaluateTime(() => this._logger2.Info(msg), out double EvaluatedTimeMS);
+                resultTest.Add(EvaluatedTimeMS);
+            }
+
+            Console.WriteLine();
+            this.PrintResults(resultTest);
+            this.PrintAccumulatedResults(resultTest);
+        }
+
+        private void PrintAccumulatedResults(List<double> times)
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"accumulated time: {times.Average().ToString("f2")} ms.");
+            Console.ResetColor();
+        }
+
+        private void PrintResults(List<double> times)
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            for (int i = 0; i < times.Count; i++)
+            {
+                Console.WriteLine($"{i}-try: {times[i]} ms.");
+            }
+            Console.ResetColor();
+        }
+
+        private void PrintHeader(string header)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("-----------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(header);
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+    }
+}
--- ELogLib/ELogBenchmark/Program.cs
+++ ELogLib/ELogBenchmark/Program.cs
@@ -2,29 +2,44 @@
 using BenchmarkDotNet.Running;
 using ELogBenchmark.CustomRun;
 using ELogLib;
+using NLog;
 
 namespace ELogBenchmark
 {
     public class Program
     {
         private static readonly Logus _logger = new Logus();
+        private static readonly ILogger _logger2 = LogManager.GetCurrentClassLogger();
         
         public static void Main(string[] args)
         {
+            _logger.Type = ELogLib.Enums.LoggerType.Heavy;
+
             /*_logger.Debug("debug message two three four fifi acpijpj ashdiha kideiwrui ijasdfij", ELogLib.Enums.PrintLevel.One);
             _logger.Trace("trace message two three four fifi acpijpj ashdiha kideiwrui ijasdfij iahjurfij iajsfdi aisud asdj ijoasjd oij 123 9578239 aslkjdhadhlah 3907490 oasdhfio", ELogLib.Enums.PrintLevel.Two);
             _logger.Info("info message two three four fifi acpijpj ashdiha kideiwrui ijasdfij iahjurfij iajsfdi aisud asdj ijoasjd oijjasdfij iahjurfij iajsfdi aisud asdj ijoasjd oijjasdfij iahjurfij iajsfdi aisud asdj ijoasjd oij", ELogLib.Enums.PrintLevel.Three);
             _logger.Warning("warning ONesolidwordmfas980124jm9aisFBDvcsd9c82oifjc82hdl8cdy72oichu2978ye39oiu2038fjo2ru2oifu0289efjp02iekf2ef2df2", ELogLib.Enums.PrintLevel.Four);
             _logger.Debug("debug darling amazing brilliant terrible kasdhoahjsdouqahdoahsdoahsdkuhaduhasdkauhdkhasidaisduaiusd", ELogLib.Enums.PrintLevel.Two);
-            _logger.Info("infosdaid98u12480cu1m0we89u01294uf0rhjf0e28rywde908fy930erhe9f8h20830u02c8ewu2908fjie983h293hf9237y info?", ELogLib.Enums.PrintLevel.One);*/
+            _logger.Info("infosdaid98u12480cu1m0we89u01294uf0rhjf0e28rywde908fy930erhe9f8h20830u02c8ewu2908fjie983h293hf9237y info?", ELogLib.Enums.PrintLevel.One);
 
-            /*_logger.Warning("kurwa\n\r\tone,\n\r\ttwo");
-            _logger.Debug("just a message.");*/
+            _logger2.Debug("debug message two three four fifi acpijpj ashdiha kideiwrui ijasdfij");
+            _logger2.Trace("trace message two three four fifi acpijpj ashdiha kideiwrui ijasdfij iahjurfij iajsfdi aisud asdj ijoasjd oij 123 9578239 aslkdhadhlah 3907490 oasdhfio");
+            _logger2.Info("info message two three four fifi acpijpj ashdiha kideiwrui ijasdfij iahjurfij iajsfdi aisud asdj ijoasjd oijjasdfij iahjurfij iajsfdi aisud asdj ijoasjd oijjasdfij iahjurfij iajsfdi aisud asdj ijoasjd oij");
+            _logger2.Warn("warning ONesolidwordmfas980124jm9aisFBDvcsd9c82oifjc82hdl8cdy72oichu2978ye39oiu2038fjo2ru2oifu0289efjp02iekf2ef2df2");
+            _logger2.Debug("debug darling amazing brilliant terrible kasdhoahjsdouqahdoahsdoahsdkuhaduhasdkauhdkhasidaisduaiusd");
+            _logger2.Info("infosdaid98u12480cu1m0we89u01294uf0rhjf0e28rywde908fy930erhe9f8h20830u02c8ewu2908fjie983h293hf9237y info?");*/
+
+            /*_logger2.Warn("kurwa\n\r\tone,\n\r\ttwo");
+            _logger2.Debug("just a message.");*/
 
             /*var summary = BenchmarkRunner.Run<ELogBenchmark.Benchmarks.ELogTests>();*/
 
-            ELogTests2 eLogTests2 = new ELogTests2();
-            eLogTests2.Run();
+            /*ELogTests2 eLogTests2 = new ELogTests2();
+            eLogTests2.Run();*/
+            NLogTests NLogTesetsdf = new NLogTests();
+            NLogTesetsdf.Run();
+
+
         }
     }
 }
